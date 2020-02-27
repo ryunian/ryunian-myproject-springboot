fc684636c104b799f5ba74e2579b2dc40fab55ea
ryunian <kls6221@naver.com>
2020-02-28 08:44:26 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=ryunian-myproject-springboot
+PROJECT_NAME=myproject
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl ryunian-myproject-springboot | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl myproject | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -31,6 +31,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-smtp.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-smtp.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profiles.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
