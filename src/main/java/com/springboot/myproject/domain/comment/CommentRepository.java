5a3e4fb4f9d80913663e45653fb27f73e7725448
ryunian <kls6221@naver.com>
2020-03-04 15:22:40 +0900

    update

--- src/main/java/com/springboot/myproject/domain/comment/CommentRepository.java
+++ src/main/java/com/springboot/myproject/domain/comment/CommentRepository.java
@@ -9,18 +9,18 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Modifying
-    @Query(value = "INSERT INTO Comment(author,content,grp) VALUES (?1,?2,(SELECT IFNULL(MAX(id)+1,1) FROM Comment c))", nativeQuery = true)
+    @Query(value = "INSERT INTO comment(author,content,grp) VALUES (?1,?2,(SELECT IFNULL(MAX(id)+1,1) FROM comment c))", nativeQuery = true)
     void write(String author, String content);
 
     @Modifying
-    @Query(value = "INSERT INTO Comment(author,content,grp,step,indent) VALUES (?1, ?2, ?3 ,?4 ,?5)", nativeQuery = true)
+    @Query(value = "INSERT INTO comment(author,content,grp,step,indent) VALUES (?1, ?2, ?3 ,?4 ,?5)", nativeQuery = true)
     void reply(String author, String content, int grp, int step, int indent);
 
     @Modifying
-    @Query(value = "UPDATE Comment set step = step+1 where grp = ?1 and step > ?2", nativeQuery = true)
+    @Query(value = "UPDATE comment set step = step+1 where grp = ?1 and step > ?2", nativeQuery = true)
     void stepModify(int grp, int step);
 
-    @Query(value = "SELECT * FROM Comment ORDER BY grp DESC, step ASC", nativeQuery = true)
+    @Query(value = "SELECT * FROM comment ORDER BY grp DESC, step ASC", nativeQuery = true)
     List<Comment> list();
 }
 
--- src/main/java/com/springboot/myproject/domain/user/RegisteredUserRepository.java
+++ src/main/java/com/springboot/myproject/domain/user/RegisteredUserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface RegisteredUserRepository extends JpaRepository<RegisteredUser,Long> {
     Optional<RegisteredUser> findById(String id);
 
-    @Query("SELECT user FROM RegisteredUser user WHERE user.id = ?1") //and user.pw = ?2")
+    @Query("SELECT user FROM RegisteredUser user WHERE user.id = ?1")
     RegisteredUser login(String id);
 
     @Query("SELECT count(user.id) From RegisteredUser user WHERE user.id = :id")
